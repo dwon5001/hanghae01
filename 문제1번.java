e2f5f65705fc87d620a20783eefcb0ff9ab561a8
dwon5001 <113032395+dwon5001@users.noreply.github.com>
2022-09-23 18:42:16 +0900

    Update 문제1번.java

--- 문제1번.java
+++ 문제1번.java
@@ -4,15 +4,15 @@
 
     public class 문제1번 {
         public static void main(String[] args) {
-            int var1 = 5;
-            int var2 = 2;
+            int var1 = 5;  //double var1 = 5;
+            int var2 = 2;  //double var2 = 2;
             double var3 = var1 / var2;
             int var4 = (int) (var3 * var2);
             System.out.println(var4);
         }
     }
 
-//    int를 double var2로 바꿔주면 된다.
+//    int를 double로 바꿔주면 된다.
 //    double var3 = var1 / var2 를 계산하면 2.5이지만
 //    var1과 var2가 정수형(int) 이기때문에 값은 정수부분인 2만 남는다.
 //    int를 double로 바꿔준다면 double var3 = var1 / var2의 값이 2.5되면서
