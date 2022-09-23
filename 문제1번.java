03945f7fcfc6e34e6722bc7db3e8b7042b8ecca0
dwon5001 <113032395+dwon5001@users.noreply.github.com>
2022-09-23 18:51:47 +0900

    Update 문제1번.java

--- 문제1번.java
+++ 문제1번.java
@@ -12,8 +12,9 @@
         }
     }
 
-//    int를 double로 바꿔주면 된다.
+//    var1과 var2의 데이터가 실수형이 아니라 정수형이기때문에 5가 아닌 4가 출력 된 것 이다
+//    정수(int)를 실수(double)로 바꿔주면 된다.
 //    double var3 = var1 / var2 를 계산하면 2.5이지만
-//    var1과 var2가 정수형(int) 이기때문에 값은 정수부분인 2만 남는다.
-//    int를 double로 바꿔준다면 double var3 = var1 / var2의 값이 2.5되면서
+//    var1과 var2가 정수(int) 이기때문에 값은 정수부분인 2만 남는다.
+//    정수(int)를 실수(double)로 바꿔준다면 double var3 = var1 / var2의 값이 2.5되면서
 //    출력 결과는 5가 나올 것 이다.
