61a3064a2045a6d4749f8defc0c49cd018ff94e7
dwon5001 <113032395+dwon5001@users.noreply.github.com>
2022-09-30 21:54:31 +0900

    Add files via upload

---
+++ SA/src/test/java/SA/SA/SaApplicationTests.java
@@ -0,0 +1,13 @@
+package SA.SA;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
